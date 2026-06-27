5c146fc6fdb46a3769c7bc98355fcffa53ec5831
Frederico Ferreira <fredericocrf87@hotmail.com>
2026-06-27 20:20:48 +0100

    Construction App for add item a cart... final update

--- Super_Management_shop_card_React/src/components/CartModal.jsx
+++ Super_Management_shop_card_React/src/components/CartModal.jsx
@@ -1,12 +1,9 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
-import Cart from './Cart';
+import Cart from "./Cart.jsx";
 
-const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
-  ref
-) {
-  const dialog = useRef();
+const CartModal = forwardRef(function Modal({ title, actions }, ref) {
+  const dialog = useRef(); 
 
   useImperativeHandle(ref, () => {
     return {
@@ -19,12 +16,12 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById('modal') // Certifica-te que este ID existe no teu index.html!
   );
 });
 
