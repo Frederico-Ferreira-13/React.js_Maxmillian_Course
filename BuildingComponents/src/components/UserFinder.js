df4dd24d9a4ee7fb7dfd9523b03bb9a24f4c3ac9
Frederico Ferreira <fredericocrf87@hotmail.com>
2026-07-07 22:30:15 +0100

    How use Functional Components in JS

---
+++ BuildingComponents/src/components/UserFinder.js
@@ -0,0 +1,55 @@
+import { Fragment, Component } from 'react';
+
+import Users from './Users';
+import classes from './UserFinder.module.css';
+import UsersContext from './user-context';
+
+class UserFinder extends Component {
+    static contextType = UsersContext;
+
+    constructor() {
+        super();
+        this.state = {
+            filteredUsers: [],
+            searchTerm: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({filteredUsers: this.context.users});
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.searchTerm !== this.state.searchTerm) {
+            this.setState({
+                filteredUsers: this.context.users.filter((user) => 
+                    user.name.includes(this.state.searchTerm)
+                ),
+            });
+        }
+    }
+
+    searchChangeHandler(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <UsersContext.Consumer>
+                    {(context) => {
+                        return (
+                            <Users users={context.users} />
+                        );
+                    }}
+                </UsersContext.Consumer>
+                <div className={classes.finder}>
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+                </div>
+                <Users users={this.state.filteredUsers} />
+            </Fragment>
+        );
+    }
+}
+
+export default UserFinder;
